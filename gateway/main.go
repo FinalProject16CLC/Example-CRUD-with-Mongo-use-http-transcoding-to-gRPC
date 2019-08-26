3c21a38dba857fef5da76e9a392d936d2ce2acaa
bigphuc <phuctd@uiza.io>
2019-08-26 12:34:06 +0700

    Remove vendor

--- gateway/main.go
+++ gateway/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	pb "Example-CRUD-with-Mongo-use-http-transcoding-to-gRPC/server/entity"
+	pb "Example-CRUD-with-Mongo-use-http-transcoding-to-gRPC/protos"
 
 	"github.com/golang/glog"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
